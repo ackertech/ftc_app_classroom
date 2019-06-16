319c2bff37b47009423ec776eaabdba7d6c396cf
jamie.acker <jamie.acker@mtbethelchristian.org>
2019-06-16 00:08:54 -0400

    Updated with Basic Scaffolding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/AutoRunner.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.robot.autonomous;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Gyro;
+
+
+@Autonomous(name = "Auto Runner")
+//@Disabled
+public class AutoRunner extends LinearOpMode {
+
+    MecanumDrive myMechDrive;
+
+    Gyro myGyro;
+
+    Paths myPaths;
+
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private static final float mmPerInch = 25.4f;
+    private static final float mmFTCFieldWidth = (12 * 6) * mmPerInch;       // the width of the FTC field (from the center point to the outer panels)
+    private static final float mmTargetHeight = (6) * mmPerInch;          // the height of the center of the target image above the floor
+
+
+    boolean targetVisible;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        final long sleepTime = 100;
+        final double SPD_DRIVE_MED = .5;
+
+
+        myMechDrive = new MecanumDrive(hardwareMap.dcMotor.get("front_left_motor"), hardwareMap.dcMotor.get("front_right_motor"), hardwareMap.dcMotor.get("rear_left_motor"), hardwareMap.dcMotor.get("rear_right_motor"));
+        myMechDrive.setLinearOp(this);
+
+        myGyro = new Gyro(hardwareMap.get(BNO055IMU.class, "imu"));
+        myGyro.setLinearOp(this);
+
+        myPaths = new Paths();
+        myPaths.setLinearOp(this);
+
+        waitForStart();
+
+        idle();
+
+        while (opModeIsActive()) {
+
+            sleep(100);
+            idle();
+
+            myPaths.DriveTest(myGyro, myMechDrive);
+
+            sleep(sleepTime);
+            idle();
+
+
+            requestOpModeStop();
+        }
+
+        idle();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/Paths.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.robot.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Gyro;
+
+
+public class Paths {
+
+    public Paths() {
+
+    }
+
+    public LinearOpMode linearOp = null;
+
+    // created constant variables that are used for speed (different setting)
+
+    final double SPD_DRIVE_LOW = .38;                  //Lowest speed
+    final double SPD_DRIVE_MED = 0.5;   //was .6 but for testing we slowed it down
+    final double SPD_DRIVE_HIGH = .75; // was .8 but for testing we slowed it down
+    final double SPD_DRIVE_MAX = 1.0;
+    final double SPD_ARM_MED = .5;
+    final long sleepTime = 50;
+
+
+
+    public void setLinearOp(LinearOpMode Op) {
+        linearOp = Op;
+    }
+
+
+    public void DriveTest (Gyro myGyro, MecanumDrive myMechDrive) {
+
+        myMechDrive.driveForward(SPD_DRIVE_HIGH, 3);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.driveBackward(SPD_DRIVE_HIGH, 3);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.rotateLeft(SPD_DRIVE_HIGH, 1);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.rotateRight(SPD_DRIVE_HIGH, 1);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.strafeRight(SPD_DRIVE_MED, 3);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.strafeRight(SPD_DRIVE_MED, 3);
+        linearOp.sleep(sleepTime);
+
+        myMechDrive.stopMotors();
+        linearOp.sleep(sleepTime);
+
+
+    }
+
+
+
+}
